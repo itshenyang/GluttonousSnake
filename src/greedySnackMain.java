1b8f2c8a7ce1d2e9d9e9c6df727c9690ca685007
sy <769797226@qq.com>
2017-09-27 12:37:24 +0800

    17.9.27

---
+++ src/greedySnackMain.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/7/27.
+ */
+
+import javax.swing.JFrame;
+
+
+public class greedySnackMain extends JFrame {
+    snackWin snackwin;
+    static final int Width = 800, Height = 600, LocX = 200, LocY = 80;
+
+    public greedySnackMain() {
+        snackwin = new snackWin();
+        add(snackwin);
+        this.setSize(Width, Height);
+        this.setVisible(true);
+        this.setLocation(LocX, LocY);
+        //snackwin.requestFocus();
+    }
+
+    public static void main(String[] args) {
+        new greedySnackMain();
+    }
+}
